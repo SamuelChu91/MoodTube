eebe15a4a7fe4de77720684451e728d5b9885df6
Samuel Chu <duhremix@gmail.com>
2019-07-30 18:09:19 -0400

    fixed attr_reader

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ApplicationRecord
     validates :username, presence: true, uniqueness: true
     validates :password_digest, :session_token, presence: true
     validates :password, length: { minimum: 6 }, allow_nil: true
-    attr_reader: password
+    attr_reader :password
     after_initialize :ensure_session_token
 
     has_many :videos
