8ee95c072ed388ce16ecffff153ee628401178be
Samuel Chu <duhremix@gmail.com>
2019-08-06 15:16:32 -0400

    finally can seed to heroku

--- frontend/components/video/video_show_item.jsx
+++ frontend/components/video/video_show_item.jsx
@@ -6,10 +6,11 @@ class VideoShowItem extends React.Component {
         if (!this.props.video) {return null};
         return (
             <div>
+                <video src=""></video>
                 {/* <h1>please</h1> */}
                 <h2>{this.props.video.title}</h2>
+                    <p>{this.props.video.description}</p>
                 {/* <video src={this.props.video.videoUrl} className="video_vid" /> */}
-                <video src=""></video>
             </div>
         );
     };
